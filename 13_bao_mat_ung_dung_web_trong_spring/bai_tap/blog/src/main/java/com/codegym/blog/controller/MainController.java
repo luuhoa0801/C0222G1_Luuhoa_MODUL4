ce1f8865519820c054a0a62d0bb784f767ac6d1e
luuhoa0801 <hoathuytien12369@gmail.com>
2022-07-04 17:15:12 +0700

    0407

---
+++ 13_bao_mat_ung_dung_web_trong_spring/bai_tap/blog/src/main/java/com/codegym/blog/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.codegym.blog.controller;
+
+import com.codegym.blog.until.WebUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "welcomePage";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Model model) {
+        return "loginPage";
+    }
+
+    @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+    public String logoutSuccessfulPage(Model model) {
+        model.addAttribute("title", "Logout");
+        return "logoutSuccessfulPage";
+    }
+
+}
